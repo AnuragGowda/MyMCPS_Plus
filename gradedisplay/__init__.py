26ab82e917335b91ed92445c06ed8e77bbe7a548
AnuragGowda <48165083+AnuragGowda@users.noreply.github.com>
2019-03-17 17:41:56 -0400

    Update __init__.py

--- gradedisplay/__init__.py
+++ gradedisplay/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_session import Session
+from flask_sslify import SSLify
 import os
 
 app = Flask(__name__)
@@ -7,5 +8,6 @@ app.config['SECRET_KEY'] = os.urandom(24)
 SESSION_TYPE = 'filesystem'
 app.config.from_object(__name__)
 Session(app)
+SSLify(app)
 
 from gradedisplay import routes
