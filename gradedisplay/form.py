65299d9236bb5b01166dd0037fdf0b2b91aafc12
anitha-gowda <50973552+anitha-gowda@users.noreply.github.com>
2019-05-23 22:03:17 -0400

    Update form.py

--- gradedisplay/form.py
+++ gradedisplay/form.py
@@ -5,7 +5,7 @@ from wtforms.validators import Length, DataRequired
 
 class LoginForm(FlaskForm):  
     username = StringField('Username', validators=[Length(min=6, max=6)], render_kw={"placeholder": "Enter your username"})
-    password = PasswordField('Password', validators=[Length(min=8, max=8)], render_kw={"placeholder": "Enter your password"})
+    password = PasswordField('Password', validators=[Length(min=6, max=8)], render_kw={"placeholder": "Enter your password"})
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
