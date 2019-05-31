9daacfe19835f5855ec6ab1cb1d55ffcd8708e66
AnuragGowda <48165083+AnuragGowda@users.noreply.github.com>
2019-05-31 19:37:11 -0400

    Update main.js

--- gradedisplay/static/main.js
+++ gradedisplay/static/main.js
@@ -1,10 +1,11 @@
-// Create an overlay variable which will be useful later, this overlay has addtional properties due to the css which is why originally it is hidden
-//var overlay = jQuery('<div id="overlay"></div>');
+// The easy stuff first 
+
 // When the submit button is clicked
 $("#submit").click(function() {
-    // Show the div, which is the overlay
+    // Show divs, in this case, it is an overlay div we care about that will be shown, created in the html
     $("div").show();
 });
+
 // This is a simple function that removes the active traits of the headers of the website
 function clearAll(){
     // Remove the about-color, this corresponds to the about button in the header
@@ -14,6 +15,7 @@ function clearAll(){
     // Remove the contact-color, this corresponds to the about button in the header
     document.getElementById("contact-color").classList.remove('active');
 }
+
 // When the page loads the following function runs
 $( document ).ready(function() {
     // First, clear all of the highlighted headers
@@ -35,33 +37,57 @@ $( document ).ready(function() {
     }
 });
 
+
+// The more complex part begins here, all of this stuff is actually used during the gradePage part of the website, so it includes the "grade caluclator" part and other stuff
+
+
+// Create a global list called headers, this is useful when showing the more specific grade info, this stuff is still easy but has to do with the harder stuff so I put it under this category
 var headers = [];
-function addDropdown(text){
+// Create a function called addDropdown that will fill in the content of the dropdowns
+function addDropdown(){
+    // Create the dropdown that we will use later
     var dropdown = '<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">';
+    // Iterate through all the headers
     for (var i = 0; i < headers.length; i++) {
-        //if(text!=headers[i]){
+        // For each header, add a dropdown item in the dropdown with the name of the header
         dropdown+="<a class=\"dropdown-item\">"+headers[i]+"</a>";
-        //}
     }
+    // Now we add the ending portion to the dropdown and return it
     return dropdown+"</div></td>"
 }
-function updateGrades(){
-    for (var i = 0; i < headers.length; i++) {
-        type = headers[i]
-        var colOne = 0,
-            colTwo = 0
-            doPercent = true
-            btn = 'success'
-            grade = 'A'
 
+// Now the actual "complex" part begins, its not really complex, just kind or lengthy, this function updates all the grades based on the info in the grade "body", so this is the grade calculator part
+function updateGrades(){
+    // Iterate through all of the headers on the page
+    for (var i = 0; i < headers.length; i++) {
+        // Set the variable type to the header, simply set the type variable to the header name
+        type = headers[i]
+        // Intitalize a bunch of variables that will be useful for this process, set colOne to 0, this col will be points earned
+        var colOne = 0,
+            // Set col2 to 0, this col represents the points possible
+            colTwo = 0
+            // A variable set to a boolean, will decide if we will need to calculate the percent later
+            doPercent = true
+            // Bootstrap button class, set it to success, we may change it later, in bootstrap, the success class is green, so buttons are set to green unless changed
+            btn = 'success'
+            // Since we set the button to green, we also set the grade to an A since an A corresponds with the greeen button in our app
+            grade = 'A'
+        // Iterate through all of the classes that have the "types", what this is doing is it is iterating through all of the dropdowns and looking for this special keyword we put in
         $('.types').each(function(index, obj) {
+            // If the text is the same as the header, meaning that if it is in the same grade category, so what we are doing here is that this nested loop is going through the grade body and then looking for a header that matches what the outer loop is currently on, so its looking for grades that are in the current category
             if ($(this).text() == type){
+                // Add the float of the points earned for that assignment to the colOne variable
                 colOne += parseFloat($(this).closest('tr').find('.one').val())
+                // And add the float of the points possible to the coltwo variable
                 colTwo += parseFloat($(this).closest('tr').find('.two').val())
             }
         });
+        // Now we change a lot of variables, I wrote this like this because this peice of code is very simple and it wastes space writing it "neatly" so I just crammed it into one line
+        // Anyway, what this line does is it sets the variables for the grade and the button color variables according to the percent the grade would be, keep in mind, we first check to see if colTwo equals 0 to aviod divison by 0 errors 
         if (colTwo == 0){doPercent = false}else if (colOne/colTwo >= .795 && colOne/colTwo < .895){btn = 'info', grade = 'B'}else if (colOne/colTwo > .695 && colOne/colTwo < .895){btn = 'primary', grade = 'C'}else if (colOne/colTwo > .595 && colOne/colTwo < .895){btn = 'warning', grade = 'D'}else if (colOne/colTwo < .595){btn = 'danger', grade = 'E'}
+        // Just a quick ternary operator to condense simple code into fewer lines, what this code does is, simply put assign percent to the percent of the grade, if doPercent is false, meaning if colTwo equals 0, we assign it 100%
         doPercent?percent = String((colOne/colTwo*100).toFixed(1))+'%':percent = '100%'
+        
         $('.header').each(function(index, obj) {
             if ($(this).text() == type){
                 $(this).closest('tr').find('button').text(grade+' - '+percent)
@@ -104,7 +130,7 @@ $(document).on('click', '#editGrades', function(){
         headers.push($(this).text())
     });
     $('.type').each(function(index, obj) {
-        $(this).before("<td><button class=\"btn btn-secondary dropdown-toggle types\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">"+$(this).text()+"</button>"+addDropdown($(this).text()));
+        $(this).before("<td><button class=\"btn btn-secondary dropdown-toggle types\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">"+$(this).text()+"</button>"+addDropdown());
     });
     $(".type").remove()
     $('.grade').each(function(index, obj) {
@@ -135,7 +161,7 @@ $(document).on('click', '#editGrades', function(){
 });
 $(document).on('click', '#addGrade', function(){
     $("#gradeTable").prepend("<tr class=\"userAdded\"><td><button type=\"button\" class=\"btn btn-danger remove\">Remove Grade</button></td>\
-    <td><button class=\"btn btn-secondary dropdown-toggle types\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">"+headers[0]+"</button>"+addDropdown(headers[0])+"</td>\
+    <td><button class=\"btn btn-secondary dropdown-toggle types\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">"+headers[0]+"</button>"+addDropdown()+"</td>\
     <td>User Added Grade</td>\
     <td style=\"width: 20%\"><form><div class=\"form-row\"><div class=\"col-sm\"><input class=\"form-control one\" type=\"number\" min=\"0\" step=\".5\" value=\"0\"></div><div class=\"col-sm\"><input class=\"form-control two\" type=\"number\" min=\"1\" step=\"1\" value=\"1\"></form></div></div></td>\
     <td><button type=\"button\" class=\"btn btn-danger gradeLetter\"\">E - 0.0%</button></td></tr>");
