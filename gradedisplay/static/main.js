2635f2a107bd0a6aa078002a56bf86ecd3c29cd5
AnuragGowda <48165083+AnuragGowda@users.noreply.github.com>
2019-06-01 14:18:08 -0400

    comments and stuff

--- gradedisplay/static/main.js
+++ gradedisplay/static/main.js
@@ -87,106 +87,152 @@ function updateGrades(){
         if (colTwo == 0){doPercent = false}else if (colOne/colTwo >= .795 && colOne/colTwo < .895){btn = 'info', grade = 'B'}else if (colOne/colTwo > .695 && colOne/colTwo < .895){btn = 'primary', grade = 'C'}else if (colOne/colTwo > .595 && colOne/colTwo < .895){btn = 'warning', grade = 'D'}else if (colOne/colTwo < .595){btn = 'danger', grade = 'E'}
         // Just a quick ternary operator to condense simple code into fewer lines, what this code does is, simply put assign percent to the percent of the grade, if doPercent is false, meaning if colTwo equals 0, we assign it 100%
         doPercent?percent = String((colOne/colTwo*100).toFixed(1))+'%':percent = '100%'
-        
+        // Now, iterate through all the headers in the html
         $('.header').each(function(index, obj) {
+            // If it matches the current header, 
             if ($(this).text() == type){
-                $(this).closest('tr').find('button').text(grade+' - '+percent)
-                $(this).closest('tr').find('button').prop("disabled", false).removeClass().addClass('btn btn-'+btn+' gradeLetter')
+                // Set the grade and the percent to what they should be, also add the proper button class, which is simply the color of the button 
+                $(this).closest('tr').find('button').text(grade+' - '+percent).addClass('btn btn-'+btn+' gradeLetter')
+                // Also, change the number in the "points" column to match the actual grade
                 $(this).closest('tr').find('.headerInfo').text(String(colOne)+'/'+String(colTwo))
             }
         });  
     }
+    // Call the function that updates the total grade, which is the overall class grade in the top right corner
     updateTotalGrade()
 }
+
+// As I said before, this is the function that updates the overall class grade
 function updateTotalGrade(){
-    var gradeVal = 0
-    var totalHead = 0
+    // Define two variables, gradeVal and totalHead, they will be helpful in calculating the total grade in the class
+    var gradeVal = 0,
+        // Declare totalHead which will represent the weight of all the items in the class
+        totalHead = 0
+    // Iterate through all of the grade headers, the purpose of this loop is to get the total weight of all the categories
     $('.header').each(function(index, obj) {
+        // As long as the grade isn't 0/0, we will do stuff with it
         if($(this).closest('tr').find('.headerInfo').text() != '0/0'){
+            // This adds the weight of that assignment to the variable to keep track of it, the reason that we do this is because, for example, if you have no grades in the graded (90%) category and a 5/5 on a homework (10%) category, you wouldn't have a 10% in the class, you would have a 100%
             totalHead += parseInt($(this).text().slice($(this).text().indexOf('(')+1,-1))
         }
     });
+    // Iterate through the headers once more, this time, we are going to do some more calculations and then we are done 
     $('.header').each(function(index, obj) {
-        var weight = $(this).text().slice($(this).text().indexOf('(')+1,-1)
-        var points = $(this).closest('tr').find('.headerInfo').text()
-        var val = eval(points)
+        // Declare some more variables that will help us, wieght is the weight of the category
+        var weight = $(this).text().slice($(this).text().indexOf('(')+1,-1),
+            // Val is simply the points earned/points possible
+            val = eval($(this).closest('tr').find('.headerInfo').text())
+        // Quick check to see if the val is not a number, incase we are doing divison by 0 again
         if (isNaN(val)){val = 0}
+        // Finally, add points according to weight to the total grade
         gradeVal += (weight*100/totalHead)*val
     }); 
+    // Check if gradeVal is not a number, this would occur if all the grades are 0/0 meaning that totalHead remains as 0, causing divison by 0, if this occurs, just set the percentage to 100
     if (isNaN(gradeVal)){gradeVal = 100}
-    grade = 'A'
-    var btn = 'success'
+    // Create new helper varibales, set grades to A
+    var grade = 'A',
+        // Set the button value to green/success again, this is the same process that we did before
+        btn = 'success'
+    // So again, we have this one liner which determines the grade and color of the button, it is only slightly different since we are sure that there is no diviosn by 0
     if (gradeVal >= 79.5 && gradeVal < 89.5){btn = 'info', grade = 'B'}else if (gradeVal > 69.5 && gradeVal < 89.5){btn = 'primary', grade = 'C'}else if (gradeVal > 59.5 && gradeVal < 89.5){btn = 'warning', grade = 'D'}else if (gradeVal < 59.5){btn = 'danger', grade = 'E'}
+    // Now we remove all the formatting classes on the button and then we make it a large button with the color and the text is the grade as well as the percent, this concludes the grade calculator portion
     $('.btn-lg').removeClass().addClass('btn btn-lg btn-'+btn).text(grade+' - '+String(gradeVal.toFixed(1))+'%')
 }
+
+// This is the part that allows users to edit their grades, this is run when the edit grades button is clicked
 $(document).on('click', '#editGrades', function(){
-    $(this).text('Add Grade');
-    $(this).attr("id","addGrade");
-    $(this).after("&nbsp;<button type=\"button\" class=\"btn btn-secondary\" id=\"undoBtn\">Undo Changes</button>");
+    // Change the text of this button (this is the button that says edit grades) to be "Add Grade", also change the id, after this button, add an undo button
+    $(this).text('Add Grade').attr("id","addGrade").after("&nbsp;<button type=\"button\" class=\"btn btn-secondary\" id=\"undoBtn\">Undo Changes</button>");
+    // We also change the column that says "Due Date" to "Keep grade?"
     $(".gradeHeader").text("Keep grade?")
-    $(".date").before("<td><button type=\"button\" class=\"btn btn-danger remove\">Remove Grade</button></td>")
-    $(".date").remove()
+    // Place a button before all classes that are marked with date and then remove them, what this essentially does is that it replaces all the dates in the column with buttons that say romve grade
+    $(".date").before("<td><button type=\"button\" class=\"btn btn-danger remove\">Remove Grade</button></td>").remove()
+    // We iterate through all the headers agian
     $('.header').each(function(index, obj) {
+        // Now we add all the header names to the headers list, this is because we only need to calculate the grades after someone changes them, so we are "paving the road" for the calculator here
         headers.push($(this).text())
     });
+    // Iterate through all the things labeled with "type", we are doing the same replacing thing that we did before with dates in the date column, only we need a loop here because we want to keep the category of the assignment so we need a more "individualized" approach
     $('.type').each(function(index, obj) {
-        $(this).before("<td><button class=\"btn btn-secondary dropdown-toggle types\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">"+$(this).text()+"</button>"+addDropdown());
+        // Before all of these, add the dropdowns which we populate as soon as we add, then we reomve the original thing
+        $(this).before("<td><button class=\"btn btn-secondary dropdown-toggle types\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">"+$(this).text()+"</button>"+addDropdown()).remove()
     });
-    $(".type").remove()
+    // Now we iterate thorugh all the grades
     $('.grade').each(function(index, obj) {
-        //alert(parseInt($(this).text().slice(0, $(this).text().indexOf('/'))));
-        var one = $(this).text().slice(0, $(this).text().indexOf('/'))
-        var two = $(this).text().slice($(this).text().indexOf('/')+1)
+        // Create variables which will be helpful later, one is equal to the points earned for a certain grade
+        var one = $(this).text().slice(0, $(this).text().indexOf('/')),
+            // Two is equal to the points possible for a certain grade
+            two = $(this).text().slice($(this).text().indexOf('/')+1)
+        // Check to see if the points possible are unentered or it is an x or z, therefore assign it the value of 0
         if(one == '' || isNaN(one)){one='0'}
-        if(two == '0' || two ==''){two == '1'}
-        $(this).before("<td style=\"width: 20%\"><form><div class=\"form-row\"><div class=\"col-sm\"><input class=\"form-control one\" type=\"number\" min=\"0\" step=\".5\" value="+one+"></div><div class=\"col-sm\"><input class=\"form-control two\" type=\"number\" min=\"1\" step=\"1\" value="+two+"></form></div></div></td>");
+        // Similarly, if two is blank or a 0, simply set it to 1 because we want to aviod divison of 0 even though we account for the user doing it anyway, since it's just good practice
+        if(two == '0' || isNaN(two)){two == '1'}
+        // Create the "number boxes" for the points earned and points possible and remove the old ones
+        $(this).before("<td style=\"width: 20%\"><form><div class=\"form-row\"><div class=\"col-sm\"><input class=\"form-control one\" type=\"number\" min=\"0\" step=\".5\" value="+one+"></div><div class=\"col-sm\"><input class=\"form-control two\" type=\"number\" min=\"1\" step=\"1\" value="+two+"></form></div></div></td>").remove();
     });
+    // Boolean that will be used later to determine if a grade update is needed
     var doUpdate = false
+    // Iterate through each button
     $('button').each(function(index, obj) {
-        if($(this).is(":disabled") || $(this).hasClass("btn-secondary")){
-            var one = $(this).closest("tr").find(".one").val()
-            var two = $(this).closest("tr").find(".two").val()
-            var doPercent = true
-            var btn = 'success'
-            var grade = 'A'
-            if (two == 0){doPercent = false}else if (one/two >= .795 && one/two < .895){btn = 'info', grade = 'B'}else if (one/two > .695 && one/two < .895){btn = 'primary', grade = 'C'}else if (one/two > .595 && one/two < .895){btn = 'warning', grade = 'D'}else if (one/two < .595){btn = 'danger', grade = 'E'}
-            doPercent?percent = String((one/two*100).toFixed(1))+'%':percent = '100%'
-            $(this).closest("tr").find('.gradeLetter').text(grade+' - '+percent)
-            $(this).closest("tr").find('.gradeLetter').prop("disabled", false).removeClass().addClass('btn btn-'+btn+' gradeLetter')
+        // If the grade is disabled, meaning if the grade is unentered, an x or a z
+        if($(this).is(":disabled")){
+            // We simply set the grade to 0, this allows the user to do whatever they want with it, if they wish to delete it or change it etc
+            $(this).closest("tr").find('.gradeLetter').text('E - 0.0%').prop("disabled", false).removeClass().addClass('btn btn-danger gradeLetter')
+            // Set do update to true since we changed some grades to 0's
             doUpdate = true
         }
     });  
+    // If anything changed, we need to update the grades again
     if(doUpdate){updateGrades()}
-    $(".grade").remove()
 });
+
+// Function that handles what happens when the add grade button is clicked
 $(document).on('click', '#addGrade', function(){
-    $("#gradeTable").prepend("<tr class=\"userAdded\"><td><button type=\"button\" class=\"btn btn-danger remove\">Remove Grade</button></td>\
-    <td><button class=\"btn btn-secondary dropdown-toggle types\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">"+headers[0]+"</button>"+addDropdown()+"</td>\
-    <td>User Added Grade</td>\
-    <td style=\"width: 20%\"><form><div class=\"form-row\"><div class=\"col-sm\"><input class=\"form-control one\" type=\"number\" min=\"0\" step=\".5\" value=\"0\"></div><div class=\"col-sm\"><input class=\"form-control two\" type=\"number\" min=\"1\" step=\"1\" value=\"1\"></form></div></div></td>\
-    <td><button type=\"button\" class=\"btn btn-danger gradeLetter\"\">E - 0.0%</button></td></tr>");
+    // Create a new row, and in that row we put the same stuff as what would be in all the other rows, set the points earned to 0 and the points possible to 1, set the description to user entered grade and also add the dropdown
+    $("#gradeTable").prepend("<tr class=\"userAdded\"><td><button type=\"button\" class=\"btn btn-danger remove\">Remove Grade</button></td><td><button class=\"btn btn-secondary dropdown-toggle types\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">"+headers[0]+"</button>"+addDropdown()+"</td><td>User Added Grade</td><td style=\"width: 20%\"><form><div class=\"form-row\"><div class=\"col-sm\"><input class=\"form-control one\" type=\"number\" min=\"0\" step=\".5\" value=\"0\"></div><div class=\"col-sm\"><input class=\"form-control two\" type=\"number\" min=\"1\" step=\"1\" value=\"1\"></form></div></div></td><td><button type=\"button\" class=\"btn btn-danger gradeLetter\"\">E - 0.0%</button></td></tr>");
     updateGrades()
 });
+
+// If they click the undo button
 $(document).on('click', '#undoBtn', function(){
+    // This just reloads the page so everything will be back to normal
     location.reload();
 });
+
+// If they click the remove button for a grade
 $(document).on('click', '.remove', function(){
+    // Remove the grade
     $(this).closest("tr").remove()
+    // Update the grades
     updateGrades()
 });
+
+// If they click any of the dropdown items for a grade
 $(document).on('click', '.dropdown-menu a', function(){
+    // Set the text of the dropdown to that value
     $(this).closest("td").find('button').text($(this).text())
+    // Update the grades
     updateGrades()
 });
+
+// If they change either of the input feilds for the grades
 $(document).on('change', '.col-sm input', function(){
-    var one = $(this).closest("form").find(".one").val()
-    var two = $(this).closest("form").find(".two").val()
-    var doPercent = true
-    var btn = 'success'
-    var grade = 'A'
+    // Create some helpful variables, this is the same process as before
+    var one = $(this).closest("form").find(".one").val(),
+        // Two again is the points possible
+        two = $(this).closest("form").find(".two").val(),
+        // The same as last time, a boolean to determine if we need to calculate the percent
+        doPercent = true,
+        // Defaults to success
+        btn = 'success',
+        // Default to a
+        grade = 'A'
+    // Again, we have the long line thing
     if (two == 0){doPercent = false}else if (one/two >= .795 && one/two < .895){btn = 'info', grade = 'B'}else if (one/two > .695 && one/two < .895){btn = 'primary', grade = 'C'}else if (one/two > .595 && one/two < .895){btn = 'warning', grade = 'D'}else if (one/two < .595){btn = 'danger', grade = 'E'}
+    // Quick ternary operator
     doPercent?percent = String((one/two*100).toFixed(1))+'%':percent = '100%'
-    $(this).closest("tr").find('.gradeLetter').text(grade+' - '+percent)
-    $(this).closest("tr").find('.gradeLetter').prop("disabled", false).removeClass().addClass('btn btn-'+btn+' gradeLetter')
+    // Change everything
+    $(this).closest("tr").find('.gradeLetter').text(grade+' - '+percent).prop("disabled", false).removeClass().addClass('btn btn-'+btn+' gradeLetter')
+    // Finally update the grades
     updateGrades()
 });
