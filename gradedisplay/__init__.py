4c8149aa12b5ffdc2c5ab01277fce866970595ad
AnuragGowda <48165083+AnuragGowda@users.noreply.github.com>
2019-06-01 17:47:41 -0400

    comments

--- gradedisplay/__init__.py
+++ gradedisplay/__init__.py
@@ -1,13 +1,23 @@
+# This is the initiation file that creates our module, here we configure our application
+# Import flask
 from flask import Flask
+# Import sessions
 from flask_session import Session
+# Import SSL connections
 from flask_sslify import SSLify
-import os
+# Import the os module
+from os import urandom
 
+# Create the flask app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(24)
-SESSION_TYPE = 'filesystem'
-app.config.from_object(__name__)
+# Configure the secret key, which is needed for the application
+app.config['SECRET_KEY'] = urandom(24)
+# Set the session type to filesystem because the default system won't work for our purposes
+app.config['SESSION_TYPE'] = 'filesystem'
+# Active sessions for the app
 Session(app)
+# Same with SSL connections, this also tries to force an SSL connection
 SSLify(app)
 
+# Import the routes here 
 from gradedisplay import routes
--- gradedisplay/form.py
+++ gradedisplay/form.py
@@ -1,11 +1,18 @@
+# Import flask forms from flask_wtf
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
+# Import the types of feilds that we want to have, a string, password, submit and boolean feild
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+# Also get some validators that check length and the data required one as well
 from wtforms.validators import Length, DataRequired
 
+# Create a new object that inherits the flaskform properties, we want to add our additional attributes that this object should have
 class LoginForm(FlaskForm):  
+    # We want a uername feild, we restrict the length and put in a placeholder
     username = StringField('Username', validators=[Length(min=6, max=6)], render_kw={"placeholder": "Enter your username"})
+    # Same as the username feild, we want a passord feild in which we restrict the length and add a placeholder
     password = PasswordField('Password', validators=[Length(min=6, max=8)], render_kw={"placeholder": "Enter your password"})
+    # Add a remember me feild
     remember = BooleanField('Remember Me')
+    # Add a submit feild
     submit = SubmitField('Login')
 
