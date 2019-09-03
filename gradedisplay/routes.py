fde98d6513015dcd5297662a6f53ede8424f189d
AnuragGowda <48165083+AnuragGowda@users.noreply.github.com>
2019-09-03 17:00:07 -0400

    Update routes.py

--- gradedisplay/routes.py
+++ gradedisplay/routes.py
@@ -154,7 +154,7 @@ def getInfo():
             # Simply tell the user that the login was unsuccessful
             flash('Login Unsuccessful, Try Again.', 'danger')
     else:
-        flash(login, 'info')
+        flash(str(dir(login)), 'info')
     # This section here tells the app to send the user the home page when they connect to our website
     return render_template('home.html', title = 'Login', form=login)
 
